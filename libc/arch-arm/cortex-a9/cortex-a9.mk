7d45e66851b6a631293b1ba6cae9d5e2652abb39
Jake Weinstein <xboxlover360@gmail.com>
2014-09-11 20:04:21 -0500

    libc: Include Krait memmove for NEON targets; also use Linaro memchr
    Change-Id: I2b2eed8fe150a782203d653a119f56d75c9141ca

--- libc/arch-arm/cortex-a9/cortex-a9.mk
+++ libc/arch-arm/cortex-a9/cortex-a9.mk
@@ -3,7 +3,8 @@ $(call libc-add-cpu-variant-src,MEMSET,arch-arm/cortex-a9/bionic/memset.S)
 $(call libc-add-cpu-variant-src,STRCAT,arch-arm/cortex-a9/bionic/strcat.S)
 $(call libc-add-cpu-variant-src,STRCMP,arch-arm/cortex-a9/bionic/strcmp.S)
 ifeq ($(ARCH_ARM_HAVE_NEON),true)
-$(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/bionic/memmove.S)
+$(call libc-add-cpu-variant-src,MEMMOVE,arch-arm/krait/bionic/memmove.S)
+$(call libc-add-cpu-variant-src,BCOPY,)
 else
 $(call libc-add-cpu-variant-src,MEMMOVE,bionic/memmove.c.arm)
 $(call libc-add-cpu-variant-src,BCOPY,string/bcopy.c.arm)
@@ -12,5 +13,6 @@ $(call libc-add-cpu-variant-src,STRCPY,arch-arm/cortex-a9/bionic/strcpy.S)
 $(call libc-add-cpu-variant-src,STRLEN,arch-arm/cortex-a9/bionic/strlen.S)
 $(call libc-add-cpu-variant-src,__STRCAT_CHK,arch-arm/cortex-a9/bionic/__strcat_chk.S)
 $(call libc-add-cpu-variant-src,__STRCPY_CHK,arch-arm/cortex-a9/bionic/__strcpy_chk.S)
+$(call libc-add-cpu-variant-src,MEMCHR,arch-arm/cortex-a15/bionic/memchr.S)
 
-#include bionic/libc/arch-arm/generic/generic.mk
+include bionic/libc/arch-arm/generic/generic.mk
