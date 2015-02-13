e6aa95f5f2e523ab8aca419ec4b67e28dc77ef68
Jake Weinstein <xboxlover360@gmail.com>
2015-02-13 21:34:30 +0000

    libc: add stpcpy support for scorpion
    Change-Id: Ia68b0dc8f180b9ec87eca6a293a403d2894e0e8b

--- libc/arch-arm/scorpion/scorpion.mk
+++ libc/arch-arm/scorpion/scorpion.mk
@@ -10,6 +10,7 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/memcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
+    arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strlen.S \
     arch-arm/cortex-a15/bionic/__strcat_chk.S \
     arch-arm/cortex-a15/bionic/__strcpy_chk.S \
