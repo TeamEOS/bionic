e9d4c9507f2d98fd15e2e3a969447a87451947b2
Yingshiuan Pan <yingshiuan.pan@linaro.org>
2015-03-18 16:08:05 +0800

    libc: arm: a15: add optimized memcpy implementation
    This optimization is extracted from cortex-strings and bionic-ized, then
    applied to Cortex-A15 cpu. This optimization requires NEON for armv7-a or
    vfpv2 for armv6. NEON is optional in cortex-a9, so I did not change
    cortex-a9.mk.
    
    This new implementation does not have __memcpy_chk(), so __memcpy_chk.cpp
    is included for arch which uses the new implementation.
    
    Change-Id: If3e11f9df4694dee3d3d8a5297817d1bc2a41d6e
    Signed-off-by: Yingshiuan Pan <yingshiuan.pan@linaro.org>

--- libc/arch-arm/cortex-a15/cortex-a15.mk
+++ libc/arch-arm/cortex-a15/cortex-a15.mk
@@ -1,6 +1,7 @@
 libc_bionic_src_files_arm += \
     arch-arm/bionic/memchr.v7a.S \
-    arch-arm/cortex-a15/bionic/memcpy.S \
+    bionic/__memcpy_chk.cpp \
+    arch-arm/bionic/memcpy.neon.S \
     arch-arm/cortex-a15/bionic/memset.S \
     arch-arm/cortex-a15/bionic/stpcpy.S \
     arch-arm/cortex-a15/bionic/strcat.S \
